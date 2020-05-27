7f6c0f8952c5b12aeb2f4455a4837c918299244c
Alexandre01010 <56257011+Alexandre01010@users.noreply.github.com>
2020-05-27 11:04:07 +0100

    Update and rename userView.js to UserView.js

---
+++ Projeto I/js/views/UserView.js
@@ -0,0 +1,53 @@
+import UserController from '../controllers/UserController.js'
+
+export default class UserView {
+    constructor() {
+        this.userController = new UserController()
+
+        this.loginForm = document.getElementById('btnSubmit');
+        this.loginUsername = document.getElementById('txtEmail');
+        this.loginPassword = document.getElementById('txtPassword');
+        this.loginMessage = document.getElementById('mdlLoginMessage');
+
+        //Take logout button
+        this.logOutBtn = document.querySelector('#btnLogout')
+        //this.bindLogOutEvent()//Quando alteramos a posição durante a execução funciona...talvez por causa do refresh?
+        this.bindAddLoginFrom()
+    }
+
+    bindAddLoginFrom() {
+        this.loginForm.addEventListener('click', event => {
+
+            event.preventDefault()
+            try {
+                this.userController.loginUser(this.loginUsername.value, this.loginPassword.value)
+                this.displayLoginMessage("User logged with success", 'success')
+
+                setTimeout(() => {
+                    location.href = "../html/activities.html";
+                },
+                    1000)
+
+
+            } catch (e) {
+                this.displayLoginMessage(e, 'danger')
+            }
+        })
+    }
+    displayLoginMessage(message, type) {
+        this.loginMessage.innerHTML =
+            `<div class="alert alert-${type}" role="alert">${message}</div>`;
+    }
+
+    bindLogOutEvent() {
+
+        this.logOutBtn.addEventListener('click', event => {
+            console.log("entrou");
+            this.userController.logOutUser()
+            setTimeout(() => {
+                location.href = "../index.html"
+            },
+                1000)
+        })
+    }
+}
