a2a36dc71470d876f9e195f504b55e800af89e15
Alexandre01010 <56257011+Alexandre01010@users.noreply.github.com>
2020-05-29 17:20:04 +0100

    Update UserModel.js

--- Projeto I/js/models/UserModel.js
+++ Projeto I/js/models/UserModel.js
@@ -7,12 +7,37 @@ export default class UserModel {
         return this.users
     }
 
-    login(username) {
-        sessionStorage.setItem('loggedUser', username)
+    registerUser(email, username, password, location, genre, weight, birthDate, aboutUser, height,photo){
+        const user = {
+            id: this.users.length > 0 ? this.users[this.users.length - 1].id + 1 : 1,
+            email: email,
+            username: username,
+            password: password,
+            location: location,
+            genre: genre,
+            weight: weight,
+            birthDate: birthDate,
+            aboutUser: aboutUser,
+            height: height,
+            photo: photo
+        }
+        this.users.push(user)
+        this._persist()
+
     }
+    _persist(){
+        localStorage.setItem('users', JSON.stringify(this.users));
+    }
+
+    login(email) {
+        sessionStorage.setItem('loggedUser', email)
+        
+    }
+    
     //Faz logout do utilizador
     logOut() {
-        sessionStorage.removeItem("loggedUser")
+        window.sessionStorage.removeItem("loggedUser")
+        
         console.log("fez logout");
 
     }
