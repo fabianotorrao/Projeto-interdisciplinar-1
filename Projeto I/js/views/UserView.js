bed4780e81c580c976550155327a120dc257ad2f
Alexandre01010 <56257011+Alexandre01010@users.noreply.github.com>
2020-05-29 17:18:20 +0100

    Update UserView.js

--- Projeto I/js/views/UserView.js
+++ Projeto I/js/views/UserView.js
@@ -1,18 +1,48 @@
 import UserController from '../controllers/UserController.js'
+import UserModel from '../models/UserModel.js'
 
 export default class UserView {
     constructor() {
         this.userController = new UserController()
 
-        this.loginForm = document.getElementById('btnSubmit');
-        this.loginUsername = document.getElementById('txtEmail');
-        this.loginPassword = document.getElementById('txtPassword');
-        this.loginMessage = document.getElementById('mdlLoginMessage');
+        this.loginForm = document.querySelector('#btnSubmit');
+        this.loginEmail = document.querySelector('#txtEmail');
+        this.loginPassword = document.querySelector('#txtPasswordLogin');
+        this.loginMessage = document.querySelector('#mdlLoginMessage');
 
         //Take logout button
         this.logOutBtn = document.querySelector('#btnLogout')
+
+
         //this.bindLogOutEvent()//Quando alteramos a posição durante a execução funciona...talvez por causa do refresh?
+
+
         this.bindAddLoginFrom()
+
+        //Identificar tag <a> em que é inserido o nome do utilizador que fez login
+        this.UserName = document.querySelector('#userControls')
+        
+        
+
+        //Take signup button
+        this.registerPhoto = document.querySelector("#photoUpload")
+        this.registerEmail = document.querySelector("#txtEmail_Register")
+        this.registerUserName = document.querySelector("#txtUsername_Register")
+        this.registerPassword = document.querySelector("#txtpassword_Register")
+        this.registerConfirmPassword = document.querySelector("#txtConfirmpassword_Register")
+        this.registerLocation = document.querySelector("#txtLocation_Register")
+        this.registerGenre = document.querySelector("#Genre_Register")
+        this.registerheight = document.querySelector("#txtheight_Register")
+        this.registerWeight = document.querySelector("#txtweight_Register")
+        this.registerBornDate = document.querySelector("#dBirth_Register")
+        this.registerAboutYou = document.querySelector("#txtAboutme_Register")
+        this.btnSubmitRegister = document.querySelector("#btnRegister")
+        this.registerMessage = document.querySelector("#mdlRegisterMessage");
+        //this.imgUrl = getBase64Image( this.registerPhoto)
+        
+        this.bindRegisterUser()
+
+        this.bindUserNameTake()
     }
 
     bindAddLoginFrom() {
@@ -20,11 +50,11 @@ export default class UserView {
 
             event.preventDefault()
             try {
-                this.userController.loginUser(this.loginUsername.value, this.loginPassword.value)
+                this.userController.loginUser(this.loginEmail.value, this.loginPassword.value)
                 this.displayLoginMessage("User logged with success", 'success')
 
                 setTimeout(() => {
-                    location.href = "../html/activities.html";
+                    window.location.href = "../html/activities.html";
                 },
                     1000)
 
@@ -36,18 +66,52 @@ export default class UserView {
     }
     displayLoginMessage(message, type) {
         this.loginMessage.innerHTML =
-            `<div class="alert alert-${type}" role="alert">${message}</div>`;
+            `<div class="alert alert-${type} d-flex justify-content-center" role="alert">${message}</div>`;
+    }
+
+    displayRegisterMessage(message, type) {
+        this.registerMessage.innerHTML =
+            `<div class="alert alert-${type} d-flex justify-content-center" role="alert">${message}</div>`;
     }
 
     bindLogOutEvent() {
 
         this.logOutBtn.addEventListener('click', event => {
-            console.log("entrou");
             this.userController.logOutUser()
             setTimeout(() => {
-                location.href = "../index.html"
+                window.location.href = "../index.html"
             },
                 1000)
+        })
+    }
+
+    bindUserNameTake() {
+        this.UserName.innerHTML = sessionStorage.getItem("loggedUser")
+    }
+
+    bindRegisterUser(){
+        this.btnSubmitRegister.addEventListener('click', event =>{
+            console.log("Clicou")
+            event.preventDefault()
+           try{
+            const read = new FileReader()
+            let file = document.querySelector('input[type=file]').files[0];
+            if(this.registerPassword.value !== this.registerConfirmPassword.value) {
+                throw Error('Password and Confirm Password are not equal');
+            }else{
+                if(this.registerUserName.value === "" || this.registerEmail.value === "" || this.registerPassword.value === "" || this.registerConfirmPassword.value === "" || this.registerLocation.value === "" || this.registerGenre.value === "" || this.registerWeight.value === "" || this.registerBornDate.value === "" || this.registerAboutYou.value === "" || this.registerheight.value === ""){
+                    throw Error('Please field all the fields');
+                }else{
+                    this.userController.createUser(this.registerEmail.value, this.registerUserName.value, this.registerPassword.value, this.registerLocation.value, this.registerGenre.value, this.registerWeight.value, this.registerBornDate.value, this.registerAboutYou.value, this.registerheight.value, this.registerPhoto.value);
+                    this.displayRegisterMessage('User registered with success!', 'success');
+                   
+                }
+            }
+
+           } catch(e){
+               this.displayRegisterMessage(e,"danger")
+
+           }
         })
     }
 }
