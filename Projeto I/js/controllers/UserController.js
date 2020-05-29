1393f963faf5e3832841c190869f0a7f33ea069a
Alexandre01010 <56257011+Alexandre01010@users.noreply.github.com>
2020-05-29 17:19:20 +0100

    Update UserController.js

--- Projeto I/js/controllers/UserController.js
+++ Projeto I/js/controllers/UserController.js
@@ -1,15 +1,21 @@
 import UserModel from '../models/UserModel.js'
-
 export default class UserController {
     constructor() {
         this.userModel = new UserModel()
     }
-    loginUser(username, password) {
-        if (this.userModel.getAll().some(user => { return user.username === username && user.password === password })) {
-            this.userModel.login(username);
+    loginUser(email, password) {
+        if (this.userModel.getAll().some(user => { return user.email === email && user.password === password})) {
+            this.userModel.login(email);
             return true;
         } else {
-            throw Error('Invalid login!');
+            throw Error('Email ou Password errado! Por favor tente novamente');
+        }
+    }
+    createUser(email, username, password, location, genre, weight, birthDate, aboutUser, height, photo){
+        if (!this.userModel.getAll().some(user => user.email === email)){
+            this.userModel.registerUser(email, username, password, location, genre, weight, birthDate, aboutUser, height, photo)
+        }else{
+            throw Error(`User with email "${email}" already exists!`)
         }
     }
 
