3c5ddabbc7e54ee071273365e2c79a1586fd9ed9
Alexandre01010 <56257011+Alexandre01010@users.noreply.github.com>
2020-06-01 16:40:05 +0100

    Update UserView.js

--- Projeto I/js/views/UserView.js
+++ Projeto I/js/views/UserView.js
@@ -9,7 +9,7 @@ export default class UserView {
         this.loginEmail = document.querySelector('#txtEmail');
         this.loginPassword = document.querySelector('#txtPasswordLogin');
         this.loginMessage = document.querySelector('#mdlLoginMessage');
-
+        
         //Take logout button
         this.logOutBtn = document.querySelector('#btnLogout')
 
@@ -17,15 +17,27 @@ export default class UserView {
         //this.bindLogOutEvent()//Quando alteramos a posição durante a execução funciona...talvez por causa do refresh?
 
 
-        this.bindAddLoginFrom()
-
         //Identificar tag <a> em que é inserido o nome do utilizador que fez login
         this.UserName = document.querySelector('#userControls')
         
-        
+        try{
+            this.bindAddLoginFrom()
+        }catch(error){
+            console.log(error,"Button not found");
+            
+        }
+
+        try{
+            this.bindLogOutEvent()
+        }catch(error){
+            console.log(error,"button not found");
+            
+        }
+
 
         //Take signup button
-        this.registerPhoto = document.querySelector("#photoUpload")
+        this.registerLoadPhotoRegister = document.querySelector("#userPhoto")
+        this.registerPhoto = document.querySelector("#txtUser_photo")
         this.registerEmail = document.querySelector("#txtEmail_Register")
         this.registerUserName = document.querySelector("#txtUsername_Register")
         this.registerPassword = document.querySelector("#txtpassword_Register")
@@ -39,10 +51,25 @@ export default class UserView {
         this.btnSubmitRegister = document.querySelector("#btnRegister")
         this.registerMessage = document.querySelector("#mdlRegisterMessage");
         //this.imgUrl = getBase64Image( this.registerPhoto)
-        
-        this.bindRegisterUser()
 
-        this.bindUserNameTake()
+        this.getUserPhoto = document.querySelector("#userPhoto")
+
+        try{
+            this.bindRegisterUser()
+        }catch(error){
+            console.log(error,"button not found");
+            
+        }
+        try{
+            this.bindUserNameTake()
+        }catch(error){
+            console.log(error,"binduserNameTake not found");
+            
+        }
+        
+       
+
+        
     }
 
     bindAddLoginFrom() {
@@ -52,7 +79,7 @@ export default class UserView {
             try {
                 this.userController.loginUser(this.loginEmail.value, this.loginPassword.value)
                 this.displayLoginMessage("User logged with success", 'success')
-
+ 
                 setTimeout(() => {
                     window.location.href = "../html/activities.html";
                 },
@@ -74,10 +101,10 @@ export default class UserView {
             `<div class="alert alert-${type} d-flex justify-content-center" role="alert">${message}</div>`;
     }
 
-    bindLogOutEvent() {
+   bindLogOutEvent() {
 
-        this.logOutBtn.addEventListener('click', event => {
-            this.userController.logOutUser()
+       this.logOutBtn.addEventListener('click', event => {
+           this.userController.logOutUser()
             setTimeout(() => {
                 window.location.href = "../index.html"
             },
@@ -86,7 +113,10 @@ export default class UserView {
     }
 
     bindUserNameTake() {
-        this.UserName.innerHTML = sessionStorage.getItem("loggedUser")
+        console.log(sessionStorage.getItem("loggedUser"))
+        this.UserName.innerHTML = sessionStorage.getItem("userName")
+        this.getUserPhoto.setAttribute("src",sessionStorage.getItem("userPhotoUser"))
+        
     }
 
     bindRegisterUser(){
@@ -94,8 +124,7 @@ export default class UserView {
             console.log("Clicou")
             event.preventDefault()
            try{
-            const read = new FileReader()
-            let file = document.querySelector('input[type=file]').files[0];
+          
             if(this.registerPassword.value !== this.registerConfirmPassword.value) {
                 throw Error('Password and Confirm Password are not equal');
             }else{
@@ -103,6 +132,7 @@ export default class UserView {
                     throw Error('Please field all the fields');
                 }else{
                     this.userController.createUser(this.registerEmail.value, this.registerUserName.value, this.registerPassword.value, this.registerLocation.value, this.registerGenre.value, this.registerWeight.value, this.registerBornDate.value, this.registerAboutYou.value, this.registerheight.value, this.registerPhoto.value);
+                    this.registerLoadPhotoRegister.setAttribute("src",this.registerPhoto.value)
                     this.displayRegisterMessage('User registered with success!', 'success');
                    
                 }
