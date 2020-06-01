385e0fcd1d5139817fd0ad1bee800dcbb11a1085
Alexandre01010 <56257011+Alexandre01010@users.noreply.github.com>
2020-06-01 16:41:26 +0100

    Update UserController.js

--- Projeto I/js/controllers/UserController.js
+++ Projeto I/js/controllers/UserController.js
@@ -4,8 +4,22 @@ export default class UserController {
         this.userModel = new UserModel()
     }
     loginUser(email, password) {
-        if (this.userModel.getAll().some(user => { return user.email === email && user.password === password})) {
-            this.userModel.login(email);
+        if (this.userModel.getAll().some(user => { return user.email === email && user.password === password})) { 
+            this.userModel.login(email); 
+           let userEmailCompare = JSON.parse(localStorage.getItem('users'))
+           
+           let userPhotoLink
+           let userNameLogged
+            for (let i = 0 ; i <= [this.userModel.users.length -1]; i++){
+                if(userEmailCompare[i].email === sessionStorage.getItem('loggedUser')){
+                    sessionStorage.setItem('userPhotoUser', this.userPhotoLink = JSON.parse(localStorage.getItem('users'))[i].photo);
+                    sessionStorage.setItem('userName', this.userNameLogged = JSON.parse(localStorage.getItem('users'))[i].username)
+                    
+                }
+              
+            }
+            //let userPhotoLink = JSON.parse(localStorage.getItem('users'))
+            
             return true;
         } else {
             throw Error('Email ou Password errado! Por favor tente novamente');
