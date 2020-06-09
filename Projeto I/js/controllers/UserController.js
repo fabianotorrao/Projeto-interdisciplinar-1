600ee41ccbbd2c67b476bdff9a5c9b89f11db176
fabianotorrao <56256925+fabianotorrao@users.noreply.github.com>
2020-06-09 02:57:32 +0100

    managing users

--- Projeto I/js/controllers/UserController.js
+++ Projeto I/js/controllers/UserController.js
@@ -4,37 +4,53 @@ export default class UserController {
         this.userModel = new UserModel()
     }
     loginUser(email, password) {
-        if (this.userModel.getAll().some(user => { return user.email === email && user.password === password})) { 
-            this.userModel.login(email); 
-           let userEmailCompare = JSON.parse(localStorage.getItem('users'))
-           
-           let userPhotoLink
-           let userNameLogged
-            for (let i = 0 ; i <= [this.userModel.users.length -1]; i++){
-                if(userEmailCompare[i].email === sessionStorage.getItem('loggedUser')){
+        if (this.userModel.getAll().some(user => { return user.email === email && user.password === password })) {
+            this.userModel.login(email);
+            let userEmailCompare = JSON.parse(localStorage.getItem('users'))
+
+            let userPhotoLink
+            let userNameLogged
+            for (let i = 0; i <= [this.userModel.users.length - 1]; i++) {
+                if (userEmailCompare[i].email === sessionStorage.getItem('loggedUser')) {
                     sessionStorage.setItem('userPhotoUser', this.userPhotoLink = JSON.parse(localStorage.getItem('users'))[i].photo);
                     sessionStorage.setItem('userName', this.userNameLogged = JSON.parse(localStorage.getItem('users'))[i].username)
-                    
+                    sessionStorage.setItem('userType', this.userNameLogged = JSON.parse(localStorage.getItem('users'))[i].type)
                 }
-              
+
             }
             //let userPhotoLink = JSON.parse(localStorage.getItem('users'))
-            
+
             return true;
         } else {
             throw Error('Email ou Password errado! Por favor tente novamente');
         }
     }
-    createUser(email, username, password, location, genre, weight, birthDate, aboutUser, height, photo, type){
-        if (!this.userModel.getAll().some(user => user.email === email)){
+    createUser(email, username, password, location, genre, weight, birthDate, aboutUser, height, photo, type) {
+        if (!this.userModel.getAll().some(user => user.email === email)) {
             this.userModel.registerUser(email, username, password, location, genre, weight, birthDate, aboutUser, height, photo, type)
-        }else{
+        } else {
+            throw Error(`User with email "${email}" already exists!`)
+        }
+    }
+    editUser(email, username, password, location, genre, weight, birthDate, aboutUser, height, photo, type,id) {
+        if (this.userModel.getAll().filter(user => user.email == email && user.id==id).length==1) {
+            console.log(this.userModel.getAll().filter(user => user.email === email).length)
+            this.userModel.editUser(email, username, password, location, genre, weight, birthDate, aboutUser, height, photo, type,id)
+        } else {
             throw Error(`User with email "${email}" already exists!`)
         }
     }
 
     logOutUser() {
         this.userModel.logOut()
+    }
+
+    getById(id) {
+        for (const user of this.userModel.users) {
+            if (user.id == id) {
+                return user
+            }
+        }
     }
 
 }
