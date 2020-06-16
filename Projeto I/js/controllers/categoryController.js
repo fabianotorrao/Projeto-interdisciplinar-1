7ecc7c10c7ac26f1505f04b0c6b0cefb6d67e2fc
fabianotorrao <56256925+fabianotorrao@users.noreply.github.com>
2020-06-16 13:35:57 +0100

    Img to category

--- Projeto I/js/controllers/categoryController.js
+++ Projeto I/js/controllers/categoryController.js
@@ -3,9 +3,9 @@ export default class UserController {
     constructor() {
         this.categoryModel = new categoryModel()
     }
-    addCategory(newcategory) {
-        if (!this.categoryModel.getAll().some(category => category === newcategory)) {
-            this.categoryModel.addCategory(newcategory)
+    addCategory(newcategory,IMG) {
+        if (!this.categoryModel.getAll().some(category => category.category === newcategory)) {
+            this.categoryModel.addCategory(newcategory,IMG)
         }
         else {
             throw Error(`category  "${newcategory}" already exists!`)
@@ -14,9 +14,9 @@ export default class UserController {
     deleteCategory(category) {
         this.categoryModel.deleteCategory(category)
     }
-    editCategory(update, old) {
-        if (update === old || (!this.categoryModel.getAll().some(category => category===update))) {
-            this.categoryModel.edit(update,old)
+    editCategory(Nphoto,update, old) {
+        if (update === old || (!this.categoryModel.getAll().some(category => category.category===update))) {
+            this.categoryModel.edit(Nphoto,update,old)
         }
         else{
             throw Error(`category ${update} already exists!`)
