9ed5c1f274b642f609110510fb5c29bf01352ad4
fabianotorrao <56256925+fabianotorrao@users.noreply.github.com>
2020-06-16 13:36:28 +0100

    Img to category

--- Projeto I/js/models/categoryModel.js
+++ Projeto I/js/models/categoryModel.js
@@ -1,24 +1,34 @@
 export default class categoryModel {
     constructor() {
 
-        this.categories = localStorage.categories ? JSON.parse(localStorage.categories) : []
+        this.categories =  localStorage.categories ? JSON.parse(localStorage.categories) : []
     }
     getAll() {
         return this.categories
     }
+    
     _persist(){
         localStorage.setItem('categories', JSON.stringify(this.categories));
     }
-    addCategory(category){
-        this.categories.push(category)
+    addCategory(category,photo){
+        const newCategory={
+            category:category,
+            photo:photo
+        }
+        
+        this.categories.push(newCategory)
         this._persist()
     }
     deleteCategory(Delcategory){
-        this.categories=this.categories.filter(category=>category!=Delcategory)
+        this.categories=this.categories.filter(category=>category.category!=Delcategory)
         this._persist()
     }
-    edit(update,old){
-        this.categories=this.categories.map(category=>category==old?update:category)
+    edit(nphoto,update,old){
+        const editCategory={
+            category:update,
+            photo:nphoto
+        }
+        this.categories=this.categories.map(category=>category.category==old?editCategory:category)
         this._persist()
     }
 
