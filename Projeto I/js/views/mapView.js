a2cd14bbfc7a686bfa8894741dd83550d78f9c22
nanishipi <gonalo78@gmail.com>
2020-06-22 02:20:18 +0100

    Admin + Map changes

--- Projeto I/js/views/mapView.js
+++ Projeto I/js/views/mapView.js
@@ -1,3 +1,38 @@
+    initMap = function(){
+    let options = {
+      zoom:8,
+      center:{lat:42.3601,lng:-71.0589}
+    }
+    
+    let map = new google.maps.Map(document.getElementById('map'), options);
+    
+  
+    renderMarkers(activitiesListParse = []);{
+      if (activitiesListParse.length != 0){
+        for (const activity of activitiesListParse){
+          addMarker(activity.local)
+        }
+      }
+    }
+
+    function addMarker(props){
+      let marker = new google.maps.Marker({
+        position:props.coords,
+        map:map,
+      });
+
+      if(props.content){
+        var infoWindow = new google.maps.InfoWindow({
+          content:props.content
+        });
+
+        marker.addListener('click', function(){
+          infoWindow.open(map, marker);
+        });
+      }
+    }
+  }
+
 
 
 
